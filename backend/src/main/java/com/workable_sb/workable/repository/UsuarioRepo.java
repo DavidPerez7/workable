a441d5b132bed781e6477e31ee0a36ec0a5f2aa6
DavidPerez7 <juan_dperez51@soy.sena.edu.co>
2025-11-30 09:14:06 -0500

    crud notificacion service

--- backend/src/main/java/com/workable_sb/workable/repository/UsuarioRepo.java
+++ backend/src/main/java/com/workable_sb/workable/repository/UsuarioRepo.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepo extends JpaRepository<Usuario, Long> {
     // Buscar usuario por correo (para login)
     Optional<Usuario> findByCorreo(String correo);
     
