2fa5cc51360e2b95ba545c7dcf91d9937a339a74
DavidPerez7 <juan_dperez51@soy.sena.edu.co>
2025-12-06 23:33:25 -0500

    chore: eliminar test innecesarios de codigo java, el profesor pidio test automatizados de http no de codigo

--- backend/src/main/java/com/workable_sb/workable/test/TestEmailRunner.java
+++
@@ -1,5 +0,0 @@
-package com.workable_sb.workable.test;
-
-// Obsoleto: la ruta de pruebas de correo fue retirada por cambio a WhatsApp.
-// Se deja la clase vacía para evitar referencias a la antigua funcionalidad de email.
-public class TestEmailRunner { }
